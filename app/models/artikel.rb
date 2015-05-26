55b92e534f3550155f6b82bb3d74272a5911894c
Marco <marco_iphone@gmx.de>
2015-05-26 15:43:52 +0200

    default scope

--- app/models/artikel.rb
+++ app/models/artikel.rb
@@ -1,4 +1,12 @@
 class Artikel < ActiveRecord::Base
   belongs_to :autor
   has_and_belongs_to_many :themas
+
+  default_scope { sortByDate }
+
+  def self.sortByDate
+    order(datum: :desc)
+  end
+
+
 end
